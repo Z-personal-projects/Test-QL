dcb1a0c6cbd26a7e121af3d4d2aa1722e33e0ad3
Koldo Zabaleta <47446924+koldozabaleta@users.noreply.github.com>
2023-07-24 11:17:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,38 @@
-password = "SASCNB·$IECBNGV"
-print(password)
+import sqlite3
+
+ 
+
+def login(username, password):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+
+ 
+
+    # Esta consulta es insegura y susceptible a inyección de SQL
+    query = f"SELECT * FROM users WHERE username='{username}' AND password='{password}'"
+
+    cursor.execute(query)
+    user = cursor.fetchone()
+    conn.close()
+
+ 
+
+    return user
+
+ 
+
+# Ejemplo de uso del código inseguro
+if __name__ == "__main__":
+    username = input("Ingrese el nombre de usuario: ")
+    password = input("Ingrese la contraseña: ")
+
+ 
+
+    user = login(username, password)
+
+ 
+
+    if user:
+        print("Inicio de sesión exitoso.")
+    else:
+        print("Nombre de usuario o contraseña incorrectos.")
